1b8b627ff085e49ac89ae24d6dfea460bcd58795
Paweł Strącała <p.stracala@exlabs.pl>
2017-09-11 20:54:23 +0200

    Add lookalikes values for dashboard search

--- lib/daashub_api/company.rb
+++ lib/daashub_api/company.rb
@@ -4,6 +4,7 @@ module DaashubAPI
       @companies = items.fetch('items', []).map { |i| new(i) }
       check_for_errors(items)
       add_fields(items['fields'])
+      add_lookalikes_fields(items['lookalikes_items'])
       @companies
     end
 
@@ -27,6 +28,15 @@ module DaashubAPI
         @companies.instance_eval { @daashub_fields = fields }
         class << @companies
           attr_reader :daashub_fields
+        end
+      end
+    end
+
+    def self.add_lookalikes_fields(fields)
+      if fields && fields.is_a?(Array)
+        @companies.instance_eval { @lookalikes_fields = fields }
+        class << @companies
+          attr_reader :lookalikes_fields
         end
       end
     end
